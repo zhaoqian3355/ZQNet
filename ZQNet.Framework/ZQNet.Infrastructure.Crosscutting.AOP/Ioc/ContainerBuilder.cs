9c6d309d1d4eb7886f06f7517a5f1206171df053
zhaoqian <qian.zhao@network.innocellence.com>
2015-12-01 18:09:15 +0800

    Adding Aop Module for ZQNet.Framework

---
+++ ZQNet.Framework/ZQNet.Infrastructure.Crosscutting.AOP/Ioc/ContainerBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZQNet.Infrastructure.Crosscutting.AOP.Ioc
+{
+    public class ContainerBuilder1:IContainerBuilder
+    {
+        public void aa()
+        {
+            var a = new ContainerBuilder();
+        }
+    }
+}
---
+++ ZQNet.Framework/ZQNet.Infrastructure.Crosscutting.AOP/Ioc/IContainerBuilderFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZQNet.Infrastructure.Crosscutting.AOP.Ioc
+{
+    public interface IContainerBuilderFactory
+    {
+        IContainerBuilder containerBuilder { get; set; }
+
+        void CreateContainerBuilder();
+    }
+}
