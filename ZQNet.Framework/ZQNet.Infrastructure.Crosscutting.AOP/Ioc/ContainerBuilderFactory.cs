716a24022f433ab4ef96e34473fdeb8b755993ea
zhaoqian <qian.zhao@network.innocellence.com>
2015-12-02 14:35:44 +0800

    vs2012 adds git widget.

--- ZQNet.Framework/ZQNet.Infrastructure.Crosscutting.AOP/Ioc/ContainerBuilderFactory.cs
+++ ZQNet.Framework/ZQNet.Infrastructure.Crosscutting.AOP/Ioc/ContainerBuilderFactory.cs
@@ -14,7 +14,7 @@ namespace ZQNet.Infrastructure.Crosscutting.AOP.Ioc
 
         public void CreateContainerBuilder()
         {
-            this.containerBuilder = new ContainerBuilder();
+            this.containerBuilder = new FacContainerBuilder();
         }
     }
 }
---
+++ ZQNet.Framework/ZQNet.Infrastructure.Crosscutting.AOP/Ioc/FacContainerBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZQNet.Infrastructure.Crosscutting.AOP.Ioc
+{
+    public class FacContainerBuilder:IContainerBuilder
+    {
+        public void aa()
+        {
+            var a = new ContainerBuilder();
+        }
+    }
+}
